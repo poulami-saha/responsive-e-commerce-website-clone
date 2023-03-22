5614402dbf838f84345ca86e332969d06f4dac05
Poulami Saha <saha.poulami91@gmail.com>
2023-03-22 16:43:05 +0100

    initial commit

---
+++ js/main1.js
@@ -0,0 +1,9 @@
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  prevArrow:".site-slider .slider-btn .prev",
+  nextArrow:".site-slider .slider-btn .next",
+});
